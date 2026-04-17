9aad8776734b0d8b61360e70e0f7e3f35801f8a3
LIU HAO <118096117+LHope04@users.noreply.github.com>
2026-04-17 17:48:53 +0800

    ota win1

---
+++ upboard/MDK-ARM/upboard_B/crc32.d
@@ -0,0 +1,4 @@
+upboard_b\crc32.o: ../Common/crc32.c
+upboard_b\crc32.o: ../Common/crc32.h
+upboard_b\crc32.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+upboard_b\crc32.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
